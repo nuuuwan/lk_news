5c2ea8e5d3fdf4b1ec6d6cdb1b081741458a833a
nuuuwan <nuuuwan@gmail.com>
2025-09-26 19:27:26 +0530

    Don't push init

--- push_to_news_lk3.sh
+++ push_to_news_lk3.sh
@@ -1,6 +1,5 @@
 
 cd ../news_lk3
-rm -rf src/news_lk3/custom_newspapers
-cp -r ../lk_news/src/news_lk3/custom_newspapers src/news_lk3/
+cp ../lk_news/src/news_lk3/custom_newspapers/*.py src/news_lk3/custom_newspapers/
 git add src/news_lk3/custom_newspapers
 code .
