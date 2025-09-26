93026acbe562821e1ae9b4258a2396118ce9e9cb
nuuuwan <nuuuwan@gmail.com>
2025-09-26 12:19:41 +0530

    minor

--- tests/test_newspapers.py
+++ tests/test_newspapers.py
@@ -8,5 +8,4 @@ class TestCase(unittest.TestCase):
     def test_single(self):
         newspaper_cls = AdaDeranaLk
         article_list = newspaper_cls.scrape()
-        print(article_list)
         self.assertGreater(len(article_list), 0)
